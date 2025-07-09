c066935ef106d2bb144248ab78bf696c78fb7ac3
Raymon Epping <your@email.com>
2025-07-09 15:50:44 +0200

    Add Vault Radar demo leak file

---
+++ radar_demo/Vault_Radar_trigger.sh
@@ -0,0 +1,21 @@
+###############################################################
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+VERSION="2.1.1"
+# Vault Radar Demo Leak Seed
+# Version: 2.1.1
+# Timestamp: 2025-07-09 15:50:44
+# Run ID: 1752069044-6499
+# Author: raymon.epping
+# Scenario: inclusivity
+###############################################################
+# [non_inclusive][low] Master branch
+# master branch
+
+# [non_inclusive][low] Blacklist
+# blacklist
+
+###############################################################
+# End of Vault Radar Demo Leak Seed
+# For educational/demo use only!
+###############################################################
